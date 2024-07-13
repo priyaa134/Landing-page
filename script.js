bdb5ee978cf8b96c65d25ff717a2c602d9a8c960
Priya <162282609+priyaa134@users.noreply.github.com>
2024-07-13 17:02:44 +0530

    Initial Commit: Add structure for landing page of Gifting site

---
+++ script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay:450,
+    reset:true
+});
+sr.reveal('.line',{delay:200, origin:'left'});
+sr.reveal('.products',{delay:200, origin:'top'});
+sr.reveal('.types',{delay:300, origin:'right'});
+
+
